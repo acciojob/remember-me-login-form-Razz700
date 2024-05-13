e968e337ae4c57d8c2e9ca54a7e703405381558b
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-05-13 19:02:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+let btn=document.getElementById('submit');
+let name=document.getElementById('username');
+let pass=document.getElementById('password');
+let check=document.getElementById('checkbox');
+let existbtn=document.getElementById('existing');
+function login(){
+    if(check.checked){
+        localStorage.setItem("Name",`${name.value}`);
+        localStorage.setItem("Password",`${pass.value}`);
+        alert("Logged in as ");
+    }
+    else{
+        localStorage.clear();
+        alert("Logged in as ");
+    }
+}
+btn.addEventListener("click",()=>{
+   if (name.value && pass.value) {
+    if (localStorage.getItem("Name")==name.value && localStorage.getItem("Password")==pass.value) {
+        existbtn.style.display="block";
+        next();
+    }
+    else{
+        login();
+    }
+    name.value="";
+    pass.value="";
+   }	
+});
+function next(){
+existbtn.addEventListener("click",()=>{
+    if (localStorage.getItem("Name")==name.value && localStorage.getItem("Password")==pass.value) {
+alert("Logged in as ");}
+});}
